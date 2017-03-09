e6666091e3f8a886fc4a25adcd34dce0799186fa
Konrad Tyma <sejseen@gmail.com>
2017-03-09 18:23:51 +0100

    Prepera tests class SPOCK

---
+++ src/test/groovy/com/beerHangout/domain/PasswordResetTokenSpec.groovy
@@ -0,0 +1,16 @@
+package com.beerHangout.domain
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * @author Konrad Tyma on 09.03.17.
+ */
+class PasswordResetTokenSpec extends Specification {
+
+  @Unroll
+  "should calculate expiration date from #dateNow after #expirationTimeInMinutes [minutes]"() {
+    //TODO use tabelar spock formule
+  }
+
+}
---
+++ src/test/groovy/com/beerHangout/domain/authorise/utils/SecurityUtilitySpec.groovy
@@ -0,0 +1,21 @@
+package com.beerHangout.domain.authorise.utils
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * @author Konrad Tyma on 09.03.17.
+ */
+class SecurityUtilitySpec extends Specification {
+
+  @Unroll
+  "should generate #password of 10 signs"() {
+
+  }
+
+  @Unroll
+  "should return #expectedResult, when #generatedPassword contains #signs signs"() {
+
+  }
+
+}
