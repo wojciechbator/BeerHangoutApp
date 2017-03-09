5ce857e2ac81b4a116d39d07bc277fa63df0981f
Konrad Tyma <sejseen@gmail.com>
2017-03-09 17:29:03 +0100

    Scheme for newUser controller

---
+++ src/main/java/com/beerHangout/domain/login/NewAccountController.java
@@ -0,0 +1,34 @@
+package com.beerHangout.domain.login;
+
+import com.beerHangout.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Locale;
+
+/**
+ * @author Konrad Tyma on 09.03.17.
+ */
+public class NewAccountController {
+
+	@Autowired
+	private MailConstructor mailConstructor;
+
+	@Autowired
+	private JavaMailSender mailSender;
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserSecurityService securityService;
+
+	@RequestMapping("/createAccount")
+	public String newUser(Model model, Locale locale, @RequestParam("token") String token) {
+
+		return null;
+	}
+
+}
