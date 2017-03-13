820ebc29146b0d8d3e3c4cc796706b932bab9203
Konrad Tyma <sejseen@gmail.com>
2017-03-13 22:52:54 +0100

    Unit test for passwordtoken in SPOCK

--- src/test/groovy/com/beerHangout/domain/PasswordResetTokenSpec.groovy
+++ src/test/groovy/com/beerHangout/domain/PasswordResetTokenSpec.groovy
@@ -1,5 +1,6 @@
 package com.beerHangout.domain
 
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -8,9 +9,36 @@ import spock.lang.Unroll
  */
 class PasswordResetTokenSpec extends Specification {
 
+
+  @Shared
+    calendar
+
+  void setup() {
+    calendar = Calendar.getInstance()
+    calendar.setTimeInMillis(new Date().getTime())
+  }
+
   @Unroll
   "should calculate expiration date from #dateNow after #expirationTimeInMinutes [minutes]"() {
-    //TODO use tabelar spock formule
+    given:
+    def passwordResetToken = new PasswordResetToken(token, user)
+    def expectedExpirationDate
+
+    when:
+    calendar.add(Calendar.MINUTE, expirationTimeInMinutes)
+    expectedExpirationDate = new Date(calendar.getTime().getTime())
+    passwordResetToken.updateToken(token, expirationTimeInMinutes)
+
+    then:
+    //toString ignore little difference in [ms]
+    passwordResetToken.getExpirationDate().toString() == expectedExpirationDate.toString()
+
+    where:
+    user       | token         | expirationTimeInMinutes | dateNow
+    new User() | "1_testToken" | 24 * 60                 | new Date()
+    new User() | "2_testToken" | 48 * 60                 | new Date()
+    new User() | "3_testToken" | 14 * 60                 | new Date()
+    new User() | "4_testToken" | 44 * 60                 | new Date()
   }
 
 }
