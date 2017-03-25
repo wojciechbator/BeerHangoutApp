102b6f7c293b1fafb04208122169a0985c6f5fe3
wojciechbator <wojciech.jan.bator@gmail.com>
2017-03-25 22:13:15 +0100

    working geolocation, just click on map and let it locate you

--- src/main/js/components/presentation/HangoutsMap.js
+++ src/main/js/components/presentation/HangoutsMap.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
  
-const markerPosition = [51.10, 17.03];
-
 export default class HangoutsMap extends Component {
-                
-// onLocationFound = (event) => {
-//   var radius = event.accuracy;
-//   L.marker(event.latlng).addTo(Map)
-//       .bindPopup("Jesteś w promieniu " + radius + " metrów od markera.").openPopup();
-//   L.circle(event.latlng, radius).addTo(Map);
-//   Map.center = event.latlng;
-// }
+  state = {
+    hasLocation: false,
+    latlng: {
+      lat: 51.10,
+      lng: 17.03,
+    },
+  }
 
-//   componentDidMount() {
-//     this.onLocationFound();
-//   }
+  handleClick = () => {
+    this.refs.map.leafletElement.locate()
+  }
 
+  handleLocationFound = (e) => {
+    this.setState({
+      hasLocation: true,
+      latlng: e.latlng,
+    })
+  }
   render() {
+    const marker = this.state.hasLocation ? (
+      <Marker position={this.state.latlng}>
+        <Popup>
+          <span>Tutaj jesteś</span>
+        </Popup>
+      </Marker>
+    ) : null;
+
     return (
-      <Map center={markerPosition} zoom={13} style={{position:"fixed"}}>
+      <Map 
+        center={this.state.latlng} 
+        zoom={13} 
+        style={{position:"fixed"}}
+        onClick={this.handleClick}
+        onLocationfound={this.handleLocationFound}
+        ref='map'
+        >
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={markerPosition}>
-          <Popup>
-            <span>Wrocław<br/>Będzie się działo.</span>
-          </Popup>
-        </Marker>
+        {marker}
       </Map>
     )
   }
