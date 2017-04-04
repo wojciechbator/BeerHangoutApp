3fa6b7ee4251758e5a6762c504ca03563d989d5c
wojciechbator <wojciech.jan.bator@gmail.com>
2017-04-04 22:53:35 +0200

    [front] Now this branch is proper - started from master with no commits from docker :)

--- src/main/js/components/styles/styles.js
+++ src/main/js/components/styles/styles.js
@@ -2,33 +2,34 @@ export default {
   universal: {},
   comment: {
     commentsContainer: {
-        backgroundColor: 'rgba(0, 0, 0, 0.7)',
-        padding: 15
+      backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      padding: 15
 
-      },
-    singleComment:{
-        fontSize: 15,
-        fontWeight: 400,
-        overflowWrap: 'break-word'
+    },
+    singleComment: {
+      fontSize: 15,
+      fontWeight: 400,
+      overflowWrap: 'break-word'
     },
     header: {
-        marginTop: 12,
-        color: '#FFFFFF'
+      marginTop: 12,
+      color: '#FFFFFF'
     },
     commentsList: {
-        padding: 0,
-        color: '#ffffff',
+      padding: 0,
+      color: '#ffffff',
       listStyleType: 'none',
     },
     pipeStyle: {
       fontWeight: 200,
       marginLeft: 12,
       marginRight: 12,
-        color: '#bbbbbb'
+      color: '#bbbbbb'
     },
     nameAndDate: {
       fontWeight: 200,
-        color: '#bbbbbb'
+      color: '#bbbbbb',
+      overflowWrap: 'break-word'
     }
   },
   place: {
