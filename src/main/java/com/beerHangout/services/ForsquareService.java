31da6a679b2b9ce99129cabdb66248112a3a1f0e
Adam Krysiak <a.krysiak15@gmail.com>
2017-05-13 13:45:42 +0200

    Refactored forsquareService

--- src/main/java/com/beerHangout/services/ForsquareService.java
+++ src/main/java/com/beerHangout/services/ForsquareService.java
@@ -1,94 +1,63 @@
 package com.beerHangout.services;
 
 import com.beerHangout.models.Venue;
-import com.fasterxml.jackson.core.*;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.*;
+import com.beerHangout.models.VenuesSearchResponse;
 import com.google.gson.*;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonParseException;
-import fi.foyt.foursquare.api.*;
-import fi.foyt.foursquare.api.entities.CompactVenue;
+import fi.foyt.foursquare.api.FoursquareApiException;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.json.JSONException;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Adam Krysiak on 09.05.17.
  */
+
 public class ForsquareService {
-    RestTemplate restTemplate;
-    Gson gson = new GsonBuilder().registerTypeAdapter(VenuesSearchResponse.class, new VenuesSearchDeserializer()).create();
+    private static final String FORSQUARE_SEARCH_API = "api.foursquare.com/v2/venues/search";
+    private static final String KEY = "RWASTDDQUJ0RHIANAJKWLCCME1NN5TP4YJG5WGXR5B0RH4MI";
+    private final Gson gson = new GsonBuilder().registerTypeAdapter(VenuesSearchResponse.class, new VenuesSearchDeserializer()).create();
 
-    private final String FORSQUARE_URL = "https://api.foursquare.com/v2/venues/search?ll=51.11,17.06&oauth_token=RWASTDDQUJ0RHIANAJKWLCCME1NN5TP4YJG5WGXR5B0RH4MI&v=20170509";
-
-//    public List<Venue> downloadVenues() throws IOException {
-////        Map<String, String> params = new HashMap<>();
-////        params.put("ll", "51.11,17.03");
-////        params.put("oauth_token", "RWASTDDQUJ0RHIANAJKWLCCME1NN5TP4YJG5WGXR5B0RH4MI");
-////        params.put("v", "20170509");
-//        FoursquareApi foursquareApi = new FoursquareApi("423697222", "a.krysiak15@gmail.com", );
-//        ResponseEntity<VenueGroup> entity = restTemplate.postForEntity(FORSQUARE_URL,"", VenueGroup.class);
-////        ArrayList body = entity.getBody();
-//        return null;
-//    }
-
-    public void venuesSearch() throws IOException, FoursquareApiException {
+    public List<Venue> venuesSearch(String locations) throws IOException, FoursquareApiException, URISyntaxException {
         CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        HttpPost httpPost = new HttpPost(FORSQUARE_URL);
-        CloseableHttpResponse response = httpClient.execute(httpPost);
-        HttpEntity entity = response.getEntity();
-        String json = EntityUtils.toString(entity);
+        URI uri = buildURI(locations);
+        CloseableHttpResponse response = httpClient.execute(new HttpPost(uri));
+        String json = getJson(response);
         httpClient.close();
         VenuesSearchResponse venuesSearchResponse = gson.fromJson(json, VenuesSearchResponse.class);
+        return venuesSearchResponse.getVenues();
+    }
 
+    private String getJson(CloseableHttpResponse response) throws IOException {
+        HttpEntity entity = response.getEntity();
+        return EntityUtils.toString(entity);
+    }
 
+    private URI buildURI(String locations) throws URISyntaxException {
+        return new URIBuilder().setScheme("https").setHost(FORSQUARE_SEARCH_API)
+                .addParameter("oauth_token", KEY)
+                .addParameter("ll", locations)
+                .addParameter("v", "20170509")
+                .build();
     }
 
 
     public static void main(String[] args) {
-//        String ll = args.length > 0?args[0]:"44.3,37.2";
-
         ForsquareService forsquareService = new ForsquareService();
         try {
-            forsquareService.venuesSearch();
-        } catch (IOException e) {
+            forsquareService.venuesSearch("51.11,17.06").forEach(System.out::println);
+        } catch (IOException | FoursquareApiException | URISyntaxException e) {
             e.printStackTrace();
-        } catch (FoursquareApiException e) {
-            e.printStackTrace();
-        }
-
-
-    }
-
-
-    public void searchVenues() throws FoursquareApiException {
-        FoursquareApi foursquareApi = new FoursquareApi("35IRT3R4PXFDNYOWFLS0EEXRDVORLOTJMQ2TOMZTQ2JVTYMU", "STCWKLS4BSAIOYMVZIB0B1ZCRSML1PMCY2SJOXVE440QGB2J", "https://api.foursquare.com/v2/venues/search?ll=51.11,17.06");
-        Map<String, String> params = new HashMap<>();
-        params.put("ll", "50.3,17.2");
-        params.put("v", "20170509");
-    }
-
-    private class VenuesSearchResponse {
-        private List<Venue> venues;
-        public VenuesSearchResponse(List<Venue> venues) throws JSONException {
-            this.venues = venues;
-        }
-
-        public List<Venue> getVenues() {
-            return venues;
         }
     }
 
@@ -97,17 +66,21 @@ public class ForsquareService {
         public VenuesSearchResponse deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
             JsonArray jsonVenuesArray = deserializeVenuesArray(jsonElement);
             List<Venue>venues = new ArrayList<>();
-            jsonVenuesArray.forEach(e->venues.add(gson.fromJson(e, Venue.class)));
+            jsonVenuesArray.forEach(e->venues.add(convertElementToVenue(e)));
             return new VenuesSearchResponse(venues);
         }
 
         private JsonArray deserializeVenuesArray(JsonElement jsonElement) {
-            JsonObject jsonObject = jsonElement.getAsJsonObject();
-            JsonElement response = jsonObject.get("response");
-            JsonElement jsonVenuesElement = response.getAsJsonObject().get("venues");
-            return jsonVenuesElement.getAsJsonArray();
+            return jsonElement
+                    .getAsJsonObject()
+                    .get("response")
+                    .getAsJsonObject()
+                    .get("venues")
+                    .getAsJsonArray();
+        }
+
+        private Venue convertElementToVenue(JsonElement e) {
+            return gson.fromJson(e, Venue.class);
         }
     }
 }
-
-//https://api.foursquare.com/v2/   venues/search   ?ll=40.7,-74&oauth_token=RWASTDDQUJ0RHIANAJKWLCCME1NN5TP4YJG5WGXR5B0RH4MI&v=20170509
