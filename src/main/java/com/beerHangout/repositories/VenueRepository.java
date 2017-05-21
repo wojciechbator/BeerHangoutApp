3405d31826fe019ecac6b5da3d15c85926e606dd
Adam Krysiak <a.krysiak15@gmail.com>
2017-05-21 12:53:05 +0200

    removed unnecessary import

--- src/main/java/com/beerHangout/repositories/VenueRepository.java
+++ src/main/java/com/beerHangout/repositories/VenueRepository.java
@@ -1,8 +1,8 @@
 package com.beerHangout.repositories;
 
 import com.beerHangout.models.Venue;
-import fi.foyt.foursquare.api.entities.Location;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
 import java.util.List;
 
 public interface VenueRepository extends MongoRepository<Venue, String> {
